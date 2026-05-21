d4bdf9be4a5abc5907f64c4c4e8b8881e0f7dbce
ikramikram-spec <ikram.bourichi.94@edu.uiz.ac.ma>
2026-05-21 15:22:47 +0100

    describe what you did

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function showlogin() {
+        return view('auth.login');
+    }
+
+    public function login(Request $request) { ;}
+}
